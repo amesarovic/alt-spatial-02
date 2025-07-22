78a169fd0b439df34c05c02ed4836c8bdbab5054
mesarovic.andre@prophecy.io <mesarovic.andre@prophecy.io>
2025-07-22 18:36:22 +0000

    Added PolyBuild gem and sample pipeline

--- models/CreatePoint/model_CreatePoint_create_geo_point.sql
+++ models/CreatePoint/model_CreatePoint_create_geo_point.sql
@@ -1,7 +1,7 @@
 {{
   config({    
     "materialized": "table",
-    "alias": "prophecy_tmp__mdetr7aq__CreatePoint__create_geo_point",
+    "alias": "prophecy_tmp__mdett2d2__CreatePoint__create_geo_point",
     "database": "andre_dev",
     "schema": "alteryx_spatial"
   })
--- pipelines/PolyBuild.py
+++ pipelines/PolyBuild.py
@@ -1,4 +1,15 @@
 Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
 
 with DAG(Schedule = Schedule):
-    pass
+    us_states_lines = Task(
+        task_id = "us_states_lines", 
+        component = "Dataset", 
+        writeOptions = {"writeMode" : "overwrite"}, 
+        table = {"name" : "us_states_lines", "sourceType" : "Seed"}
+    )
+    model_PolyBuild_PolyBuild_1 = Task(
+        task_id = "model_PolyBuild_PolyBuild_1", 
+        component = "Model", 
+        modelName = "model_PolyBuild_PolyBuild_1"
+    )
+    us_states_lines.out >> model_PolyBuild_PolyBuild_1.in_0
