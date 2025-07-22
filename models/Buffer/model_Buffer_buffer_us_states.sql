f3eb997dc10b8de656cb13c5bf7054e0d4103e4e
mesarovic.andre@prophecy.io <mesarovic.andre@prophecy.io>
2025-07-22 17:14:19 +0000

    Added Buffer pipeline with seed data

---
+++ models/Buffer/model_Buffer_buffer_us_states.sql
@@ -0,0 +1,34 @@
+{{
+  config({    
+    "materialized": "table",
+    "alias": "prophecy_tmp__mdesmdzb__Buffer__buffer_us_states",
+    "database": "andre_dev",
+    "schema": "alteryx_spatial"
+  })
+}}
+
+WITH us_states_lines AS (
+
+  SELECT * 
+  
+  FROM {{ ref('us_states_lines')}}
+
+),
+
+buffer_us_states AS (
+
+  {{
+    andre_spatial_09.Buffer(
+      'us_states_lines', 
+      [{ "name": "name", "dataType": "String" }, { "name": "geometry", "dataType": "String" }], 
+      'geometry', 
+      25, 
+      'miles'
+    )
+  }}
+
+)
+
+SELECT *
+
+FROM buffer_us_states
